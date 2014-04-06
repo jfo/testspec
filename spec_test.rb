d059e41c2a382ba25b78a113d1b7684c8e6c2d23
urthbound <jeffowler@gmail.com>
2014-04-06 13:06:27 -0400

    frame

---
+++ spec_test.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+
+class Spec_test < Test::Unit::TestCase
+
+  def setup
+  end
+
+  def test_spec
+  end
+
+  def teardown
+  end
+
+end
---
+++ test_spec.rb
@@ -0,0 +1,14 @@
+require_relative "spec_test.rb"
+
+describe Spec_test do
+
+  before do
+  end
+
+  it do
+  end
+
+  after do
+  end
+
+end
