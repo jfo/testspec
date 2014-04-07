053479f3459e5fee514626996d0622a67ae0b1dc
urthbound <jeffowler@gmail.com>
2014-04-06 22:08:34 -0400

    dancing in the coffeebean

--- spec_test.rb
+++ spec_test.rb
@@ -3,13 +3,20 @@ require 'test/unit'
 class Spec_test < Test::Unit::TestCase
 
   def setup
-    system("rspec test_spec.rb > spec") if !File.exist?("spec")
+    if File.open("spec").read == ""
+      system("echo test > test")
+      system("rspec test_spec.rb > spec")
+    end if File.exist?("spec")
   end
 
   def test_spec
+    if File.open("spec").read != ""
+      puts "spec_test!\n"
+    end if File.exist?("spec")
   end
 
   def teardown
+    system("rspec test_spec.rb > spec") if !File.exist?("spec")
   end
 
 end
--- test_spec.rb
+++ test_spec.rb
@@ -1,15 +1,24 @@
-gtequire_relative "spec_test.rb"
+require_relative "spec_test.rb"
 
 describe Spec_test do
 
   before do
-    system("ruby spec_test.rb > test") if !File.exist?("test")
+    if File.open("test").read == ""
+      system("echo spec > spec")
+      system("ruby spec_test.rb > test")
+    end if File.exist?("test")
   end
 
-  it do
+  it 'tests the spec' do
+    if File.open("test").read != ""
+      # File.open("test").read.should include "1 tests, 0 assertions, 0 failures, 0 errors, 0 skips\n"
+      puts "test_spec!\n"
+    end if File.exist?("test")
   end
 
   after do
+    system("ruby spec_test.rb > test") if !File.exist?("test")
+    # system("rspec test_spec.rb") if !File.exist?("test") && File.open("test").read != ""
   end
 
 end
