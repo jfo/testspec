45e111592cedf8ea2c04e035f7ba1b96a136f71e
urthbound <jeffowler@gmail.com>
2014-04-06 23:49:42 -0400

    spectesttesttest

--- spec_test.rb
+++ spec_test.rb
@@ -4,19 +4,18 @@ class Spec_test < Test::Unit::TestCase
 
   def setup
     if File.open("spec").read == ""
-      system("echo test > test")
-      system("rspec test_spec.rb > spec")
+      system("echo spec > spec ; rspec test_spec.rb > spec")
     end if File.exist?("spec")
   end
 
   def test_spec
     if File.open("spec").read != ""
-      puts "spec_test!\n"
+      assert true
     end if File.exist?("spec")
   end
 
   def teardown
-    system("rspec test_spec.rb > spec") if !File.exist?("spec")
+    system("touch test ; touch spec ; rspec test_spec.rb > spec") if !File.exist?("spec")
   end
 
 end
--- test_spec.rb
+++ test_spec.rb
@@ -4,21 +4,22 @@ describe Spec_test do
 
   before do
     if File.open("test").read == ""
-      system("echo spec > spec")
-      system("ruby spec_test.rb > test")
+      system("echo test > test ; ruby spec-test.rb > test")
     end if File.exist?("test")
   end
 
   it 'tests the spec' do
     if File.open("test").read != ""
-      # File.open("test").read.should include "1 tests, 0 assertions, 0 failures, 0 errors, 0 skips\n"
-      puts "test_spec!\n"
+      true.should eq(true)
     end if File.exist?("test")
   end
 
   after do
-    system("ruby spec_test.rb > test") if !File.exist?("test")
-    # system("rspec test_spec.rb") if !File.exist?("test") && File.open("test").read != ""
+      system("touch spec ; touch test ; ruby spec_test.rb > test") if !File.exist?("test")
   end
 
 end
+
+
+
+# File.open("test").read.should include "1 tests, 0 assertions, 0 failures, 0 errors, 0 skips\n"
